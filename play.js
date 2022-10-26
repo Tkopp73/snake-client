1c0135eec0caa4eec3be2c86094c5edc42905047
Tim Kopplin <timkopplin@hotmail.com>
2022-10-26 16:44:41 +0000

    first commit snake game

---
+++ play.js
@@ -0,0 +1,24 @@
+const net = require("net");
+
+//establishes connection
+const connect = () => {
+
+  const conn = net.createConnection({
+    host: '165.227.47.243',
+    port: 50541
+  });
+  
+  //interpret incoming data as text
+  conn.setEncoding("utf8");
+
+  //returns server data
+  conn.on("data", (data) => {
+    console.log("Server says: ", data);
+  });
+
+  return conn;
+};
+
+console.log("connecting ... ");
+connect();
+
