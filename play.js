e51695a827a810f50963c49ab945134635bdaf5b
Tim Kopplin <timkopplin@hotmail.com>
2022-10-26 21:51:42 +0000

    snake game runs

--- play.js
+++ play.js
@@ -2,9 +2,9 @@ const connect = require('./client');
 const setupInput = require('./input');
 
 console.log("connecting ... ");
-connect();
+let conn = connect();
 
-setupInput();
+setupInput(conn);
 
 
 
