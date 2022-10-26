97556e723ecc836da3e89bdf053f7ec31ed8324e
Tim Kopplin <timkopplin@hotmail.com>
2022-10-26 18:32:11 +0000

    updating the snake game

--- play.js
+++ play.js
@@ -1,6 +1,21 @@
 const connect = require('./client');
 
-
 console.log("connecting ... ");
-connect();
 
+const setupInput = () => {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding("utf8");
+  stdin.resume();
+  return stdin;
+};
+
+const handleUserInput = () => {
+  if (key === '\u0003') {
+    process.exit();
+  };
+};
+
+process.stdin.on("data", handleUserInput);
+
+connect();
